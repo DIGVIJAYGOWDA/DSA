2f584df719bade9c7a4f1f1e399fa54723659324
DIGVIJAYGOWDA <rdigvijaygowda05@gmail.com>
2026-08-16 10:44:38 +0530

    Time: 27 ms (53.11%) | Memory: 26 MB (39.52%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
+    
         hashset=set()
         for n in nums:
             if n in hashset:
